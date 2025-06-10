52568735d236183ea14d72232988e04c90288b37
HN721 <hosea1422@gmail.com>
2025-06-10 16:10:14 +0700

    feat:update function forgotPassword nad adding validate

--- service/login.go
+++ service/login.go
@@ -13,6 +13,7 @@ type LoginData struct {
 	password string
 }
 type newPassword struct {
+	email    string
 	password string
 }
 
@@ -20,16 +21,19 @@ var choose int
 
 func (a LoginData) HandleLogin() {
 	if len(Data1) >= 1 {
-		if a.email == Data1[0].email && a.password == Data1[0].password {
-			fmt.Println("Login Berhasil")
-		} else {
-			fmt.Println("Login Failed Wrong email or Password")
-			fmt.Println("Forgot your Password?")
-			fmt.Scan(&choose)
-			if choose == 1 {
-				NewPassword()
+		for x := range Data1 {
+			if a.email == Data1[x].email && a.password == Data1[x].password {
+				fmt.Println("Login Berhasil")
+			} else {
+				fmt.Println("Login Failed Wrong email or Password")
+				fmt.Println("Forgot your Password?")
+				fmt.Scan(&choose)
+				if choose == 1 {
+					NewPassword()
+				}
 			}
 		}
+
 	} else {
 		fmt.Println("Silahkan Registerasi Terlebih Dahulu")
 		Register()
@@ -37,8 +41,15 @@ func (a LoginData) HandleLogin() {
 
 }
 func (s newPassword) Forgot() string {
-	Data1[0].password = s.password
 
+	for x := range Data1 {
+		if Data1[x].email == s.email {
+			Data1[x].password = s.password
+
+		} else {
+			fmt.Println("Harap Masukan Email Yang benars")
+		}
+	}
 	return s.password
 }
 func Login() {
@@ -61,9 +72,13 @@ func Login() {
 }
 func NewPassword() {
 	var password string
+	var email string
+	fmt.Println("Masukan Email:")
+	fmt.Scan(&email)
 	fmt.Print("Masukan Password: ")
 	fmt.Scan(&password)
 	result := &newPassword{
+		email:    email,
 		password: encrypt(password),
 	}
 	result.Forgot()
